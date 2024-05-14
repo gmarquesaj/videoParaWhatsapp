7d5fd969a68db015fddbf357ff7aaa868481915c
gmarquesaj <gmarquesaj@gmail.com>
2024-05-14 18:28:43 -0300

    fix

--- cortes.py
+++ cortes.py
@@ -5,7 +5,8 @@ import platform
 import sys
 
 sistema=platform.system()
-
+conversoes=[]
+videos=[]
 # Definir como o script de conversão sera criado
 arqSaida="converter."
 if(sistema=='Windows'):
@@ -40,8 +41,10 @@ segs=25
 
 # Para cada arquivo encontrado, cria uma pasta com o nome do arquivo, usa ffprobe para saber a duraçao do arquivo
 # e ffmpeg para criar copias de no maximo "segs" segundos, de todo o arquivo, convertendo se necessario para uso no whatsapp
+
 for f in arqs:
     nome=f[:len(f)-4]
+    videos.append(nome)
     f= "./converter/"+f
     if(os.path.isfile(f)==False):
         print("arquivo invalido \"",f,"\"")
@@ -51,25 +54,27 @@ for f in arqs:
         pedacos = max(1,math.floor(duracao/segs))
         soma = pedacos*segs
         dif = duracao - soma
-        if(sistema=='Windows'):
-            print("\n:: \"",nome,"\" | duracao : ",duracao," | pedacos : ",pedacos," soma ",soma," dif ",dif)
-            print("mkdir \".\\convertido\\"+nome+"\"")
-        else:
-            print("\n# \"",nome,"\" | duracao : ",duracao," | pedacos : ",pedacos," soma ",soma," dif ",dif)
-            print("mkdir ./convertido/"+nome)
+
         if(dif>1):
             pedacos+=1
         for i in range(0,pedacos):
             ini= timedelta(seconds=i*segs)
             fim= timedelta(seconds=i*segs+segs)
-            if(sistema=='Windows'):
-                codigo = ffc+"ffmpeg -ss "+str(ini)+" -to "+str(fim)+" -i \""+f+"\"  -c:a mp3 -c:v "+encoder+" -vf \"scale=-2:720\" -b:v 6000k \"./convertido/"+nome+"/"+nome+"_"+str(i+1)+".mp4\" -y"
-            else:
-                codigo = ffc+"ffmpeg -ss "+str(ini)+" -to "+str(fim)+" -vaapi_device /dev/dri/renderD128 -i \""+f+"\"  -c:a mp3 -c:v "+encoder+" -vf \"format=nv12,hwupload,scale_vaapi=-2:720\"  -f mp4 -rc_mode 1 -qp 20 \"./convertido/"+nome+"/"+nome+"_"+str(i+1)+".mp4\" -y"
+            conversoes.append({"duracao":str(duracao),"pedacos":str(pedacos),"nome":nome,"pedaco":i+1,"soma":str(soma),"dif":str(dif),"inicio":str(ini),"fim":str(fim),"entrada":"\""+f+"\"","saida":"\"./convertido/"+nome+"/"+str(i+1).zfill(5) +".mp4\""})
 
-         #   codigo = "ffmpeg -ss "+str(ini)+" -to "+str(fim)+" -i \""+f+"\"  -c:a mp3 -c:v hevc_amf \"./conv/"+nome+"/"+nome+"_"+str(i+1)+".mp4\" -y"
-            print(codigo)
-#if(sistema=='Windows'):
-#    print("pause")
+
+for v in videos:
+    if(sistema=='Windows'):
+       print("mkdir .\\convertido\\"+v)
+    else:
+       print("mkdir \"./convertido/"+v+"\"")
+for v in conversoes:
+    codigo=""
+    if(sistema=='Windows'):
+        codigo = ffc+"ffmpeg -ss "+v["inicio"]+" -to "+v["fim"]+" -i "+v["entrada"]+"  -c:a mp3 -c:v "+encoder+" -vf \"scale=-2:720\" -b:v 6000k "+v["saida"]+" -y"
+    else:
+        codigo = ffc+"ffmpeg -ss "+v["inicio"]+" -to "+v["fim"]+" -vaapi_device /dev/dri/renderD128 -i "+v["entrada"]+"  -c:a mp3 -c:v "+encoder+" -vf \"format=nv12,hwupload,scale_vaapi=-2:720\"  -f mp4  -qp 20 "+v["saida"]+" -y"
+    print(codigo)
+
     
 
