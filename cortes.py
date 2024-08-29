7c231015f5b4a0d3ac67d8dd2398aaf2e838078d
gmarquesaj <gmarquesaj@gmail.com>
2024-08-29 05:07:45 -0300

    log,classe,funcoes e organização

--- cortes.py
+++ cortes.py
@@ -3,10 +3,54 @@ import math
 from datetime import timedelta
 import platform
 import sys
+from dataclasses import dataclass
+@dataclass
+class conversao:
+    inicio : str
+    fim : str
+    arqIn :str
+    arqOut :str
+    usarGpu : bool
+    duracao:str
+    pedacos:str
+    pedaco:int
+    nome:str
+    soma:str
+    dif:str
+niveisDeLog=[ "quiet"  ,"panic"  ,"fatal"  ,"error"  ,"warning","info"   ,"verbose","debug"]
+#  -------------------------------------------
+#---------------CONFIGURAÇÕES----------------------
+#--------------------------------------------------
+# resolucao
+reso:str="720"
+# Criar ou nao uma pasta para cada video
+criarPastas:bool=1
+# Duracao maxima dos pedaços
+segs:int=59
+# inverter ou nao a sequencia de arquivos
+inverter:bool=0
+#usar copy como encoder
+copiar:bool=0
+#manter o nome original do video,se nao nomear o video com um numero ordenado
+manterNomeOri:bool=0
+nivelLog:int=4
+#--------------------------------------------------
 
-sistema=platform.system()
-conversoes=[]
-videos=[]
+sistema:str=platform.system()
+
+def gerarCodigo(v:conversao):
+    encoder:str="copy" if copiar== 1 else "h264_amf" if sistema=='Windows'else "h264_vaapi" if v.usarGpu==1 else "h264"
+    device:str="" if sistema=='Windows' else " -vaapi_device /dev/dri/renderD128 "  if v.usarGpu==1 else " "
+    qualidade:str="-b:v 6000k" if sistema=='Windows' else "-qp 20" 
+    filtro:str="" if copiar== 1 else " -vf scale=-2:"+reso if sistema=='Windows' else " -vf format=nv12,hwupload,scale_vaapi=-2:"+reso if v.usarGpu==1 else " -vf format=nv12,scale=-2:"+reso
+    log:str= "" if nivelLog==-1 else "-loglevel "+niveisDeLog[nivelLog]
+    codigo:str = ffc+"ffmpeg "+log+" -ss "+v.inicio+" -to "+v.fim+" "+device+" -i "+v.arqIn+"  -c:a mp3 -c:v "+encoder+" "+ filtro+" "+qualidade+" "+v.arqOut+" -y"
+    return codigo
+def criarDiretorio(nome):
+     return "mkdir .\\convertido\\"+nome if(sistema=='Windows') else  "mkdir \"./convertido/"+nome+"\""
+ 
+
+
 # Definir como o script de conversão sera criado
 arqSaida="converter."
 if(sistema=='Windows'):
@@ -14,12 +58,6 @@ if(sistema=='Windows'):
 else:
     arqSaida+="sh"
 
-# Criar ou nao uma pasta para cada video
-criarPastas=0
-# Duracao maxima dos pedaços
-segs=59
-# inverter ou nao a sequencia de arquivos
-inverter=0
 
 file = open(arqSaida, "w")
 sys.stdout = file
@@ -33,7 +71,6 @@ else:
     print("#!/bin/sh -x")
 # Caminho dos arquivos de entrada
 arqs=os.listdir("./converter/")
-manterNomeOri=1
 # Excluir arquivos que ja foram processados antes e recriar a pasta
 if(sistema=='Windows'):
     print("rmdir /s /q convertido")
@@ -49,7 +86,7 @@ print("clear")
 vNum=0
 for f in arqs:
     nome=f[:len(f)-4]
-    videos.append(nome)
+  
     f= "./converter/"+f
     if(os.path.isfile(f)==False):
         print("arquivo invalido \"",f,"\"")
@@ -60,17 +97,19 @@ for f in arqs:
         pedacos = max(1,math.floor(duracao/segs))
         soma = pedacos*segs
         dif = duracao - soma
-        print("# NOME::",nome," DUR::",duracao," PED::",pedacos," SOMA::",soma," DIF::",dif,"\n")
+        print("\n# NOME::",nome," DUR::",duracao," PED::",pedacos," SOMA::",soma," DIF::",dif)
+        if(criarPastas==1):
+         print(criarDiretorio(nome))
         if(dif>1):
             pedacos+=1
         for i in range(0,pedacos):
             ini= timedelta(seconds=i*segs)
             fimi=i*segs+segs
             fim= timedelta(seconds=fimi)
-            usargpu=1
+            usarGpu=1
             if(math.floor(duracao)<=fimi):
                 fim= timedelta(seconds=(duracao))
-                usargpu=0
+                usarGpu=0
             if(inverter==1):
                 va=pedacos-i
             else:
@@ -78,34 +117,11 @@ for f in arqs:
             pNum=str(va).zfill(5)
 
             saida="\"./convertido/"+(nome if manterNomeOri==1 else "")+( "/" if criarPastas==1 else str(vNum).zfill(5)+"_" if manterNomeOri==0 else "_")+pNum+".mp4\"" 
-            conversoes.append({"usargpu":usargpu,"duracao":str(duracao),"pedacos":str(pedacos),"nome":nome,"pedaco":va,"soma":str(soma),"dif":str(dif),"inicio":str(ini),"fim":str(fim),"entrada":"\""+f+"\"","saida":saida})
+            conv =conversao(usarGpu=usarGpu, duracao=str(duracao), pedacos=str(pedacos),nome=nome, pedaco=va, soma=str(soma),dif=str(dif), inicio=str(ini), fim=str(fim), arqIn="\""+f+"\"", arqOut=saida)
+           # conversoes.append(conv)
+            print(gerarCodigo(conv))
 
-if(criarPastas==1):
-    for v in videos:
-        if(sistema=='Windows'):
-            print("mkdir .\\convertido\\"+v)
-        else:
-            print("mkdir \"./convertido/"+v+"\"")
-reso="720"
-#prox="" 
 
-for v in conversoes:
-    inicio=v["inicio"]
-    fim=v["fim"]
-    arqIn=v["entrada"]
-    arqOut=v["saida"]
-    usargpu=v["usargpu"]
-    usargpu=0
 
-    encoder="h264_amf" if sistema=='Windows'else "h264_vaapi" if usargpu==1 else "h264"
-   # encoder="copy"
-    device="" if sistema=='Windows' else " -vaapi_device /dev/dri/renderD128 "  if usargpu==1 else " "
-    qualidade="-b:v 6000k" if sistema=='Windows' else "-qp 20" 
-    filtro="" if encoder=="copy" else " -vf scale=-2:"+reso if sistema=='Windows' else " -vf format=nv12,hwupload,scale_vaapi=-2:"+reso if usargpu==1 else " -vf format=nv12,scale=-2:"+reso
 
-    codigo = ffc+"ffmpeg -ss "+inicio+" -to "+fim+" "+device+" -i "+arqIn+"  -c:a mp3 -c:v "+encoder+" "+ filtro+" "+qualidade+" "+arqOut+" -y"
-    print(codigo)
-
- 
-    
 
