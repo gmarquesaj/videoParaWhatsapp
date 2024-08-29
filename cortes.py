4e8f8bc22304b52db0a7faf2bb382bb10f3072c1
gmarquesaj <gmarquesaj@gmail.com>
2024-08-29 02:40:46 -0300

    garantir

--- cortes.py
+++ cortes.py
@@ -14,7 +14,12 @@ if(sistema=='Windows'):
 else:
     arqSaida+="sh"
 
+# Criar ou nao uma pasta para cada video
 criarPastas=0
+# Duracao maxima dos pedaços
+segs=59
+# inverter ou nao a sequencia de arquivos
+inverter=0
 
 file = open(arqSaida, "w")
 sys.stdout = file
@@ -28,7 +33,7 @@ else:
     print("#!/bin/sh -x")
 # Caminho dos arquivos de entrada
 arqs=os.listdir("./converter/")
-
+manterNomeOri=1
 # Excluir arquivos que ja foram processados antes e recriar a pasta
 if(sistema=='Windows'):
     print("rmdir /s /q convertido")
@@ -38,13 +43,10 @@ else:
     print("mkdir ./convertido/")
    # print("q=25")
 
-# Duracao maxima dos pedaços
-segs=25
-# inverter ou nao a sequencia de arquivos
-inverter=1
 # Para cada arquivo encontrado, cria uma pasta com o nome do arquivo, usa ffprobe para saber a duraçao do arquivo
 # e ffmpeg para criar copias de no maximo "segs" segundos, de todo o arquivo, convertendo se necessario para uso no whatsapp
 print("clear")
+vNum=0
 for f in arqs:
     nome=f[:len(f)-4]
     videos.append(nome)
@@ -53,7 +55,7 @@ for f in arqs:
         print("arquivo invalido \"",f,"\"")
     else:
         result = os.popen(ffc+'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"'+f+"\"").read()
-        
+        vNum=vNum+1
         duracao = (float(result[:len(result)-1]))
         pedacos = max(1,math.floor(duracao/segs))
         soma = pedacos*segs
@@ -73,7 +75,9 @@ for f in arqs:
                 va=pedacos-i
             else:
                 va=i
-            saida="\"./convertido/"+nome+("/"if criarPastas==1 else "_")+str(va).zfill(5) +".mp4\"" 
+            pNum=str(va).zfill(5)
+
+            saida="\"./convertido/"+(nome if manterNomeOri==1 else "")+( "/" if criarPastas==1 else str(vNum).zfill(5)+"_" if manterNomeOri==0 else "_")+pNum+".mp4\"" 
             conversoes.append({"usargpu":usargpu,"duracao":str(duracao),"pedacos":str(pedacos),"nome":nome,"pedaco":va,"soma":str(soma),"dif":str(dif),"inicio":str(ini),"fim":str(fim),"entrada":"\""+f+"\"","saida":saida})
 
 if(criarPastas==1):
@@ -82,20 +86,22 @@ if(criarPastas==1):
             print("mkdir .\\convertido\\"+v)
         else:
             print("mkdir \"./convertido/"+v+"\"")
-reso="480"
-#prox=""
+reso="720"
+#prox="" 
+
 for v in conversoes:
     inicio=v["inicio"]
     fim=v["fim"]
     arqIn=v["entrada"]
     arqOut=v["saida"]
     usargpu=v["usargpu"]
+    usargpu=0
 
     encoder="h264_amf" if sistema=='Windows'else "h264_vaapi" if usargpu==1 else "h264"
    # encoder="copy"
     device="" if sistema=='Windows' else " -vaapi_device /dev/dri/renderD128 "  if usargpu==1 else " "
-    qualidade="-b:v 6000k" if sistema=='Windows' else "-qp 25" 
-    filtro="" if encoder=="copy" else " -vf \"scale=-2:"+reso+"\"" if sistema=='Windows' else " -vf \"format=nv12,hwupload,scale_vaapi=-2:"+reso+"\"" if usargpu==1 else " -vf \"format=nv12,scale=-2:"+reso+"\""
+    qualidade="-b:v 6000k" if sistema=='Windows' else "-qp 20" 
+    filtro="" if encoder=="copy" else " -vf scale=-2:"+reso if sistema=='Windows' else " -vf format=nv12,hwupload,scale_vaapi=-2:"+reso if usargpu==1 else " -vf format=nv12,scale=-2:"+reso
 
     codigo = ffc+"ffmpeg -ss "+inicio+" -to "+fim+" "+device+" -i "+arqIn+"  -c:a mp3 -c:v "+encoder+" "+ filtro+" "+qualidade+" "+arqOut+" -y"
     print(codigo)
